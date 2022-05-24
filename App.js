9be53c5ea2c4a45dc6356ea547bfaaa049f6a460
YOGO MS CORP <yfeijoo95@gmail.com>
2022-05-24 09:50:37 -0400

    New App

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-web';
+import LoginScreen from './component/LoginScreen';
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <LoginScreen></LoginScreen>
+  
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
